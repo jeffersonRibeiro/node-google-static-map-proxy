4d08cda623c6dd97a68808d3b5912b4c1ad5b380
tvglobo <jefferson.ribeiro.contato@gmail.com>
2019-01-21 13:27:48 -0200

    FIXED!

--- app/index.js
+++ app/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 
 const { staticMap } = require('./static-map-api');
-const { download, deleteFile } = require('./util');
+const { download, ignoreFavicon, deleteFile } = require('./util');
 
 const app = express();
 
+app.use(ignoreFavicon);
+
 app.get('/:cep', (req, res) => {
   const cep = req.params.cep;
-  console.log(cep);
-  res.send(cep);
-  // download(staticMap(cep), filepath => () => {
-  //   res.sendFile(filepath);
-  //   res.on('finish', function() {
-  //     try {
-  //       deleteFile(filepath);
-  //     } catch (err) {
-  //       console.log(err, filepath);
-  //     }
-  //   });
-  // });
+  download(staticMap(cep), filepath => () => {
+    res.sendFile(filepath);
+    res.on('finish', function() {
+      try {
+        deleteFile(filepath);
+      } catch (err) {
+        console.log(err, filepath);
+      }
+    });
+  });
 });
 
 const port = process.env.PORT || 8000;
